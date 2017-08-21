58f80e7afb74ae50f6c418e3820aa5173dafa009
Brian Dang <30878065+WhaleAsauresRex@users.noreply.github.com>
2017-08-21 10:49:27 -1000

    Insult generator
    understand the math formula

---
+++ insult.js
@@ -0,0 +1,8 @@
+function sayHello() {    var insult = [
+  "You are a boobalicious lucious lobster",
+  "You should probaby start a school shooting because ur a white kid",
+  "Jesus christ you make me wish I was swallowed"
+  ,"Holy crap you're a intractable doofus"
+  ];
+var x = Math.floor(Math.random()*5);
+window.alert(insult[x]);}
