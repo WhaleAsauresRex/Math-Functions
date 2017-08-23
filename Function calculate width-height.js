d1545fa0e2efb517bd4afd3b5b2bd3bdacb501d2
Brian Dang <30878065+WhaleAsauresRex@users.noreply.github.com>
2017-08-23 10:20:57 -1000

    Function calculate
    This will allow you to multiply height/etc.

---
+++ Function calculate width-height.js
@@ -0,0 +1,8 @@
+function calculateArea(w,h){
+var area=(width*height);
+return area;
+}
+var width=prompt("Width?");
+var height=prompt("Height?");
+var wallOne = calculateArea();
+window.alert(wallOne);
---
+++ cookieclicker.js
@@ -0,0 +1,8 @@
+var score=0;
+function sayllo(){
+  score++;
+  window.alert(score);
+  if(score==10){var hello ="OMG why are you doing this?";
+    window.alert(hello);
+  }
+}
