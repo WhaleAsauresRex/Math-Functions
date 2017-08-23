a0e20c387907790d3533358d9585da1b8ee306ae
Brian Dang <30878065+WhaleAsauresRex@users.noreply.github.com>
2017-08-23 10:33:38 -1000

    Add files via upload

--- Function calculate width-height.js
+++ Function calculate width-height.js
@@ -1,8 +1,15 @@
 function calculateArea(w,h){
+  //create a function that takes two parameters
 var area=(width*height);
+//multiply width and height parameters
 return area;
+//value is returned
 }
 var width=prompt("Width?");
+//ask the user for width
 var height=prompt("Height?");
+//ask the user for height
 var wallOne = calculateArea();
+//calculate the width and height
 window.alert(wallOne);
+//calls on function to multiply the width and height
