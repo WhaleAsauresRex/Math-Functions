d1ff812dc99dc20f6835ffbc24685580ff75397d
Brian Dang <30878065+WhaleAsauresRex@users.noreply.github.com>
2017-08-30 10:32:19 -1000

    Adding data to objects
    Objects can be made with no data, but you can add more data to the object.

---
+++ ObjectsAndSubractingItems.js
@@ -0,0 +1,12 @@
+var bankAccount1 = {};
+bankAccount1["name"] = "Tony";
+bankAccount1["balance"] = 10;
+//This is how you create an object, and add stuff into it
+var bankAccount2 = {};
+bankAccount2["name"] = "Brian";
+bankAccount2["balance"] = 20;
+var x = prompt("How much do you want to withdraw?");
+//You are now trying to subtract the balance
+
+window.alert(bankAccount1.balance-=x);
+window.alert(bankAccount1.balance);
