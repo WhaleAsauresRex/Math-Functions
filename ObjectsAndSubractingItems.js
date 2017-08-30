4e2a54f3dc57f4292b1f78c3367f681bde0a0c0c
Brian Dang <30878065+WhaleAsauresRex@users.noreply.github.com>
2017-08-30 10:42:35 -1000

    Add files via upload

--- ObjectsAndSubractingItems.js
+++ ObjectsAndSubractingItems.js
@@ -7,6 +7,7 @@ bankAccount2["name"] = "Brian";
 bankAccount2["balance"] = 20;
 var x = prompt("How much do you want to withdraw?");
 //You are now trying to subtract the balance
-
+var y = +x;
+//y is changing x into an integer instead of string
 window.alert(bankAccount1.balance-=x);
 window.alert(bankAccount1.balance);
