5062c0c9be4d0d584f6ac463d3a7c41912e45a73
Brian Dang <30878065+WhaleAsauresRex@users.noreply.github.com>
2017-09-01 10:23:04 -1000

    Add files via upload

--- MrSkams.js
+++ MrSkams.js
@@ -1,10 +1,12 @@
 
-var mrSkam = prompt("Choose a number bewtween 1-6!");
+var mrSkam = prompt("Choose a number bewtween 1-6! No decimals!");
+var mrSkam = Math.floor(parseInt(mrSkam));
 if ((mrSkam<1)||(mrSkam>6)||isNaN(mrSkam)){
 window.alert("FOOLLOW THE RULLES");
 }
 else{
-var playerTwo = prompt("Choose a number between 1-6!");
+var playerTwo = prompt("Choose a number between 1-6! No decimals!");
+var playerTwo = Math.floor(parseInt(playerTwo));
 if (mrSkam==playerTwo){
 window.alert("YOU WIN");
 }
